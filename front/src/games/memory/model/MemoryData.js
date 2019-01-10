c705ca61ccd14f57887725fcefdddf1beebdbab2
I051486 <antoine.maillard@sap.com>
2019-01-10 09:11:47 +0100

    updated card paths

--- front/src/games/memory/model/MemoryData.js
+++ front/src/games/memory/model/MemoryData.js
@@ -4,181 +4,181 @@ MemoryData.ITEMS = [
   {
     id: 0,
     color: '#FFFFFF',
-    src: '_Adventure_Map.svg',
+    src: 'card_adventure_map.svg',
     text: 'memory.card.adventure-map'
   },
   {
     id: 1,
     color: '#FFFFF0',
-    src: '_Armor.svg',
+    src: 'card_armor.svg',
     text: 'memory.card.armor'
   },
   {
     id: 2,
     color: '#FFFF0F',
-    src: '_Bow_Arrow.svg',
+    src: 'card_Bow_Arrow.svg',
     text: 'memory.card.bow-arrow'
   },
   {
     id: 3,
     color: '#FFF0FF',
-    src: '_Castle.svg',
+    src: 'card_castle.svg',
     text: 'memory.card.castle'
   },
   {
     id: 4,
     color: '#FF0FFF',
-    src: '_Centaur.svg',
+    src: 'card_centaur.svg',
     text: 'memory.card.centaur'
   },
   {
     id: 5,
     color: '#F0FFFF',
-    src: '_Crystal_Shard.svg',
+    src: 'card_crystal_shard.svg',
     text: 'memory.card.crystal-shard'
   },
   {
     id: 6,
     color: '#0FFFFF',
-    src: '_Destructive_Magic.svg',
+    src: 'card_destructive_magic.svg',
     text: 'memory.card.destructive-magic'
   },
   {
     id: 7,
     color: '#FFFF00',
-    src: '_Dragon_Egg.svg',
+    src: 'card_dragon_egg.svg',
     text: 'memory.card.dragon-egg'
   },
   {
     id: 8,
     color: '#FFF00F',
-    src: '_Elf.svg',
+    src: 'card_elf.svg',
     text: 'memory.card.elf'
   },
   {
     id: 9,
     color: '#FF00FF',
-    src: '_Fairy.svg',
+    src: 'card_fairy.svg',
     text: 'memory.card.fairy'
   },
   {
     id: 10,
     color: '#F00FFF',
-    src: '_Food.svg',
+    src: 'card_food.svg',
     text: 'memory.card.food'
   },
   {
     id: 11,
     color: '#00FFFF',
-    src: '_Grim_Reaper.svg',
+    src: 'card_grim_reaper.svg',
     text: 'memory.card.grim-reaper'
   },
   {
     id: 12,
     color: '#0FFFF0',
-    src: '_Helmet.svg',
+    src: 'card_helmet.svg',
     text: 'memory.card.helmet'
   },
   {
     id: 13,
     color: '#0FFF0F',
-    src: '_Item_Bag.svg',
+    src: 'card_item_bag.svg',
     text: 'memory.card.item-bag'
   },
   {
     id: 14,
     color: '#0FF0FF',
-    src: '_King.svg',
+    src: 'card_king.svg',
     text: 'memory.card.king'
   },
   {
     id: 15,
     color: '#0F0FFF',
-    src: '_Knight.svg',
+    src: 'card_Knight.svg',
     text: 'memory.card.knight'
   },
   {
     id: 16,
     color: '#F0F0FF',
-    src: '_Medusa.svg',
+    src: 'card_medusa.svg',
     text: 'memory.card.medusa'
   },
   {
     id: 17,
     color: '#F0FF0F',
-    src: '_Monster.svg',
+    src: 'card_monster.svg',
     text: 'memory.card.monster'
   },
   {
     id: 18,
     color: '#F0FFF0',
-    src: '_Orc.svg',
+    src: 'card_orc.svg',
     text: 'memory.card.orc'
   },
   {
     id: 19,
     color: '#FF0F0F',
-    src: '_Poison_Spider.svg',
+    src: 'card_poison_spider.svg',
     text: 'memory.card.poison-spider'
   },
   {
     id: 20,
     color: '#FF0FF0',
-    src: '_Potion.svg',
+    src: 'card_potion.svg',
     text: 'memory.card.potion'
   },
   {
     id: 21,
     color: '#FFF0F0',
-    src: '_Sorceress.svg',
+    src: 'card_sorceress.svg',
     text: 'memory.card.sorceress'
   },
   {
     id: 22,
     color: '#FF0000',
-    src: '_Spell_Book.svg',
+    src: 'card_spell_book.svg',
     text: 'memory.card.spell-book'
   },
   {
     id: 23,
     color: '#0FF000',
-    src: '_Spell_Scroll.svg',
+    src: 'card_spell_scroll.svg',
     text: 'memory.card.spell-scroll'
   },
   {
     id: 24,
     color: '#00FF00',
-    src: '_Sword.svg',
+    src: 'card_sword.svg',
     text: 'memory.card.sword'
   },
   {
     id: 25,
     color: '#000FF0',
-    src: '_Treant.svg',
+    src: 'card_treant.svg',
     text: 'memory.card.treant'
   },
   {
     id: 26,
     color: '#0000FF',
-    src: '_Unicorn.svg',
+    src: 'card_unicorn.svg',
     text: 'memory.card.unicorn'
   },
   {
     id: 27,
     color: '#000F0F',
-    src: '_Viking.svg',
+    src: 'card_viking.svg',
     text: 'memory.card.viking'
   },
   {
     id: 28,
     color: '#00F00F',
-    src: '_Villager.svg',
+    src: 'card_villager.svg',
     text: 'memory.card.villager'
   },
   {
     id: 29,
     color: '#0F000F',
-    src: '_Werewolf.svg',
+    src: 'card_werewolf.svg',
     text: 'memory.card.werewolf'
   }
 ]
