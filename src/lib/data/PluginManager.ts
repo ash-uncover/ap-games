76fa6bf9f5850cfd3c645be5ff4c103454b1f80b
Antoine MAILLARD <ash.uncover@gmail.com>
2022-12-15 07:03:24 +0100

    Update PluginManager.ts

--- src/lib/data/PluginManager.ts
+++ src/lib/data/PluginManager.ts
@@ -1,5 +1,4 @@
-import { LogLevels } from "@uncover/js-utils-logger"
-import Logger from "@uncover/js-utils-logger/dist/Logger"
+import Logger, { LogLevels } from "@uncover/js-utils-logger"
 
 export interface Plugins {
   [key: string]: Plugin
@@ -51,7 +50,30 @@ const plugins: Plugins = {}
 const defines: PluginDefines = {}
 const providers: PluginProviders = {}
 
-const checkPlugin = (data: Plugin) => {
+export const fetchPlugin = async (url: string) => {
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: new Headers()
+    })
+    const data = await response.json()
+    return data
+  } catch (error) {
+    throw new Error(`[fetchPlugin] Failed to fetch plugin from ${url}: ${error}`)
+  }
+}
+
+export const checkPlugin = (data: Plugin) => {
+  if (!data) {
+    throw new Error('[checkPlugin] plugin information is not defined')
+  }
+  if (!data.name) {
+    throw new Error('[checkPlugin] plugin name is missing')
+  }
+  if (!data.url) {
+    throw new Error('[checkPlugin] plugin url is missing')
+  }
+
   if (!data.dependencies) {
     data.dependencies = []
   }
@@ -63,57 +85,65 @@ const checkPlugin = (data: Plugin) => {
   }
 }
 
-const loadPluginInternal = async (url: string, master: boolean) => {
+export const loadPluginDefines = (data: Plugin) => {
+  Object.keys(data.defines).forEach((define) => {
+    if (defines[define]) {
+      LOGGER.warn(`Define '${define}' from '${data.url}' already registered from '${plugins[data.name].url}'`)
+    } else {
+      defines[define] = data.defines[define]
+    }
+  })
+}
+
+export const loadPluginProvides = (data: Plugin) => {
+  Object.keys(data.provides).forEach((provide) => {
+    if (!defines[provide]) {
+      LOGGER.warn(`Provide '${provide}' from '${data.url}' is not defined`)
+    } else {
+      const providerData = data.provides[provide]
+      providers[provide] = providers[provide] || []
+      const provider: PluginProvider = {
+        plugin: data.name,
+        type: providerData.type,
+        url: `${data.url}${providerData.url || ''}`,
+        properties: {}
+      }
+      Object.keys(providerData.properties).forEach((property: string) => {
+        switch (defines[provide].properties[property]) {
+          case 'url': {
+            provider.properties[property] = `${data.url}${providerData.properties[property]}`
+            break
+          }
+          default: {
+            provider.properties[property] = providerData.properties[property]
+            break
+          }
+        }
+      })
+      providers[provide].push(provider)
+    }
+  })
+}
+
+export const loadPluginDependencies = (data: Plugin) => {
+  return data.dependencies.map((dependency: string) => loadPluginInternal(dependency, false))
+}
+
+export const loadPluginInternal = async (url: string, master: boolean) => {
   try {
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: new Headers()
-    })
-    const data = await response.json()
+    const data = await fetchPlugin(url)
     checkPlugin(data)
     if (plugins[data.name]) {
       LOGGER.warn(`Plugin '${data.name}' from '${data.url}' already registered from '${plugins[data.name].url}'`)
-    } else {
-      plugins[data.name] = data
+      // We dont process anything to prevent cyclic loading
+      return Promise.resolve()
     }
-    Object.keys(data.defines).forEach((define) => {
-      if (defines[define]) {
-        LOGGER.warn(`Define '${define}' from '${data.url}' already registered from '${plugins[data.name].url}'`)
-      } else {
-        defines[define] = {
-          ...data.defines[define],
-          _providers: []
-        }
-      }
-    })
-    Object.keys(data.provides).forEach((provide) => {
-      if (!defines[provide]) {
-        LOGGER.warn(`Provide '${provide}' from '${data.url}' is not defined`)
-      } else {
-        const providerData = data.provides[provide]
-        providers[provide] = providers[provide] || []
-        const provider: PluginProvider = {
-          plugin: data.name,
-          type: providerData.type,
-          url: `${data.url}${providerData.url || ''}`,
-          properties: {}
-        }
-        Object.keys(providerData.properties).forEach((property: string) => {
-          switch (defines[provide].properties[property]) {
-            case 'url': {
-              provider.properties[property] = `${data.url}${providerData.properties[property]}`
-              break
-            }
-            default: {
-              provider.properties[property] = providerData.properties[property]
-              break
-            }
-          }
-        })
-        providers[provide].push(provider)
-      }
-    })
-    return Promise.all(data.dependencies.map((dependency: string) => loadPluginInternal(dependency, false)))
+    plugins[data.name] = data
+    loadPluginDefines(data)
+    loadPluginProvides(data)
+    const dependencyLoaders = loadPluginDependencies(data)
+    return Promise.all(dependencyLoaders)
+
   } catch (error) {
     LOGGER.warn(`Failed to load plugin from '${url}'`)
     LOGGER.warn(String(error))
@@ -121,7 +151,7 @@ const loadPluginInternal = async (url: string, master: boolean) => {
 }
 
 export class PluginManager {
-  static loadPlugin (url: string) {
+  static loadPlugin(url: string) {
     return loadPluginInternal(url, true)
   }
   static get plugins() {
