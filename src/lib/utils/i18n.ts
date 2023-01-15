bb3e6059617c8e2d63b33f9fa0be5b7a98297d93
ash-uncover <ash.uncover@gmail.com>
2023-01-15 10:09:00 +0100

    Update i18n.ts

--- src/lib/utils/i18n.ts
+++ src/lib/utils/i18n.ts
@@ -3,6 +3,7 @@ import { initReactI18next } from 'react-i18next'
 
 import LanguageDetector from 'i18next-browser-languagedetector'
 import HttpBackend from 'i18next-http-backend'
+
 import CONFIG from 'config'
 
 i18n
